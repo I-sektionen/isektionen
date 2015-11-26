37e9f31ac96a758463877bec87465465444133ef
filip <c.filip.cornell@gmail.com>
2015-11-26 16:21:28 +0100

    Added blog urls

--- wsgi/isektionen_django/isektionen/urls.py
+++ wsgi/isektionen_django/isektionen/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from . import views
+from blog import urls as blog_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', view=views.index, name='index'),
     url(r'^test_page/$', view=views.show_test_page, name='page-1'),
-
     url(r'^blog/', include(blog_urls)),
     url(r'utbildningen/$',view=views.show_education_page, name='utbildningen')
 ]
