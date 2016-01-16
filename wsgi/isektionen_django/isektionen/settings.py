7cd5cd50ac16bf5ee2e65be686a8f3c954e61b7b
Isac Ekberg <isaek808@student.liu.se>
2016-01-16 11:53:36 +0100

    Allowed hosts restricted.

--- wsgi/isektionen_django/isektionen/settings.py
+++ wsgi/isektionen_django/isektionen/settings.py
@@ -22,8 +22,7 @@ ON_JENKINS = 'JENKINS_SERVER_IPORTALEN' in os.environ
 if ON_PASS:
     ALLOWED_HOSTS = [os.environ.get['OPENSHIFT_APP_DNS'],
                      socket.gethostname(),
-                     'isektionen.se',
-                     'www.isektionen.se']
+                     '.isektionen.se']
     DEBUG = False
 
 elif ON_JENKINS:
