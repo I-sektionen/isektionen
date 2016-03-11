855cb561e82892520ce0d13c1c75e63d4efce785
andyPandy1337 <andreas.gustafsson92@gmail.com>
2016-03-11 01:32:07 +0100

    Added comments to describe slide behaviour

--- wsgi/static/js/src/i-vs-Ii.js
+++ wsgi/static/js/src/i-vs-Ii.js
@@ -1,10 +1,14 @@
 var toggleProgramInfo = function() {
 
+    //saves variables to reduce DOM-traverse
     industriellEkonomiInterLink = $("#industriell-ekonomi-inter-link");
     industriellEkonomiLink = $( "#industriell-ekonomi-link" )
     industriellEkonomiInterInfo = $("#industriell-ekonomi-inter-info")
     industriellEkonomiInfo = $("#industriell-ekonomi-info")
 
+
+    // These functions are used to add appropriate classes to the navigation of the two programs.
+    // When clicking corresponding link - classes are set and removed
 industriellEkonomiInterLink.click(function() {
     industriellEkonomiInfo.addClass("no-show-left");
     industriellEkonomiInterInfo.removeClass("no-show-right");
@@ -13,7 +17,6 @@ industriellEkonomiInterLink.click(function() {
 });
 
  industriellEkonomiLink.click(function() {
-
     industriellEkonomiInterInfo.addClass("no-show-right");
     industriellEkonomiInfo.removeClass("no-show-left");
     industriellEkonomiLink.addClass("active");
