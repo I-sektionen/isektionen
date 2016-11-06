f38404be63fceafe66bbbb05d12fd94cd1d6a8cb
henkepenkepenke <henkepenkepenke@gmail.com>
2016-11-06 17:55:47 +0100

    Fixade fade-in-element m.m. på företagssidan.

--- wsgi/static/js/isektionen.js
+++ wsgi/static/js/isektionen.js
@@ -5,7 +5,37 @@ $(document).ready(function() {
   $('.expander-trigger').click(function(){
     $(this).toggleClass("expander-hidden");
   });
-});;/**
+});;$(document).ready(function() {
+
+    var fadeInElements = $('.fade-in-element');
+    $(fadeInElements).addClass('fade-element-hide');
+
+    $(window).scroll(function() {
+
+        fadeInElements.each(function () {
+
+            if ($(this).length > 0) {
+                var elementTopToPageTop = $(this).offset().top;
+                var windowTopToPageTop = $(window).scrollTop();
+                var windowInnerHeight = window.innerHeight;
+                var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+                var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+                var distanceFromBottomToAppear = 100;
+
+                if (elementTopToWindowBottom > distanceFromBottomToAppear) {
+                    $(this).addClass('fade-element-show');
+                }
+                else if (elementTopToWindowBottom < 0) {
+                    $(this).removeClass('fade-element-show');
+                    $(this).addClass('fade-element-hide');
+                }
+            }
+        });
+
+    });
+
+});
+;/**
  * Created by lehtonen on 15-11-16.
  */
 var load_instagram = function () {
