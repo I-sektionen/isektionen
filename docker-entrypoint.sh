889d4d2b4f4618133eb856bafdd34d6b9d2303af
jonathan <jonathan@jonathananderson.se>
2017-09-17 16:49:11 +0200

    Docker support

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+git pull origin docker
+/usr/bin/pip3 install -r ../requirements.txt
+export PYTHONPATH=$PYTHONPATH:/srv/wsgi:/srv/wsgi/isektionen_django
+/usr/bin/python3 isektionen_django/manage.py migrate        # Apply database migrations
+/usr/bin/python3 isektionen_django/manage.py collectstatic --noinput  # collect static files
+# Prepare log files and start outputting logs to stdout
+touch /srv/logs/gunicorn.log
+touch /srv/logs/access.log
+# tail -n 0 -f /srv/logs/*.log &
+echo Starting nginx
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn isektionen_django.isektionen.wsgi:application \
+    --name base_app \
+    --bind unix:isektionen_django.sock \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log &
+exec service nginx start
